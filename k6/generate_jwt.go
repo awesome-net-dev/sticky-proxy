f6c70f95dbf32674088584d426b4b8a3e8b97c69
awesome-net-dev <242967347+awesome-net-dev@users.noreply.github.com>
2026-03-04 16:47:23 +0100

    ci: fix lint errors

--- k6/generate_jwt.go
+++ k6/generate_jwt.go
@@ -21,8 +21,6 @@ func main() {
 		slog.Error("failed to create output file", "error", err)
 		os.Exit(1)
 	}
-	defer func() { _ = file.Close() }()
-
 	writer := csv.NewWriter(file)
 
 	_ = writer.Write([]string{"userId", "jwt"})
@@ -37,12 +35,14 @@ func main() {
 		if err != nil {
 			slog.Error("failed to sign token", "userId", i, "error", err)
 			writer.Flush()
+			_ = file.Close()
 			os.Exit(1)
 		}
 
 		if err := writer.Write([]string{fmt.Sprintf("%d", i), signed}); err != nil {
 			slog.Error("failed to write csv row", "userId", i, "error", err)
 			writer.Flush()
+			_ = file.Close()
 			os.Exit(1)
 		}
 
@@ -52,6 +52,7 @@ func main() {
 	}
 
 	writer.Flush()
+	_ = file.Close()
 	slog.Info("jwt generation complete", "file", "users.csv", "totalUsers", totalUsers)
 }
 
